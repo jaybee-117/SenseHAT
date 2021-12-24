5b01f812f576d4a6fd56a220ecc2634f4c5451c6
Jaybee-117 <unofficialme117@gmail.com>
2021-12-24 17:18:55 +0530

    Added comments

--- direction.py
+++ direction.py
@@ -2,10 +2,15 @@ from sense_hat import SenseHat
 from time import sleep
 
 sense = SenseHat()
+#Gyroscope
 sense.set_imu_config(False, True, False)                    #Enables the Gyroscope
 gyro_only = sense.get_gyroscope()
+print("g_p={pitch}; g_r={roll}; g_y={yaw}".format(**gyro_only))
 
-print("p={pitch}; r={roll}; y={yaw}".format(**gyro_only))   # Show the Gyroscopic orientation
+#Accelerometer
+sense.set_imu_config(False, False, True)                    #Enables the Accelerometer
+accel_only = sense.get_accelerometer()
+print("a_p: {pitch}, a_r: {roll}, a_y: {yaw}".format(**accel_only)) 
 
 sense.show_letter("H")                                      # Show "H"
 sleep(2)                                                    # Wait for 2s
